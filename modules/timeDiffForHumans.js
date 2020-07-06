9c11c254620bfecf08dcb89f9d47f49958ddb3a8
Radiantly <44368997+radiantly@users.noreply.github.com>
2020-07-06 22:33:46 +0530

    Compute nextUpdate time along with human diff

--- modules/timeDiffForHumans.js
+++ modules/timeDiffForHumans.js
@@ -3,6 +3,7 @@ const longStrings = {
   minute: "minute",
   hour: "hour",
   day: "day",
+  week: "week",
   and: "and",
 };
 const shortStrings = {
@@ -10,6 +11,7 @@ const shortStrings = {
   minute: "min",
   hour: "hr",
   day: "day",
+  week: "wk",
   and: "&",
 };
 
@@ -33,4 +35,53 @@ export const timeDiffForHumans = (ms, short = false) => {
   if (finalWords.length > 2) finalWords.splice(-2, 0, strings.and);
 
   return finalWords.join(" ");
+};
+
+const oneSec = 1000;
+const oneMin = 60 * oneSec;
+const oneHrs = 60 * oneMin;
+const oneDay = 24 * oneHrs;
+const oneWks = 7 * oneDay;
+
+const trackUpdate = {
+  1: oneMin,
+  2: oneHrs,
+  3: oneDay,
+};
+
+export const computeTimeDiff = (timeLeftms, short = false) => {
+  let strings = short ? shortStrings : longStrings;
+
+  if (timeLeftms < oneMin)
+    return {
+      nextUpdate: 0,
+      humanDiff: strings.lessThanAMinute,
+    };
+
+  /* Compute track:
+   * 1 -> Show days, hours & minutes
+   * 2 -> Show weeks, days & hours
+   * 3 -> Show weeks & days
+   */
+  const track = timeLeftms < 2 * oneWks ? (timeLeftms < 2 * oneDay ? 1 : 2) : 3;
+
+  const weeks = Math.floor(timeLeftms / oneWks);
+  const days = Math.floor((timeLeftms % oneWks) / oneDay);
+  const hours = Math.floor((timeLeftms % oneDay) / oneHrs);
+  const minutes = Math.floor((timeLeftms % oneHrs) / oneMin);
+
+  let finalString = "";
+  if (weeks) finalString = `${weeks} ${strings.week}${weeks > 1 ? "s" : ""} `;
+  if (days) finalString += `${days} ${strings.day}${days > 1 ? "s" : ""} `;
+  if (hours && track < 3) finalString += `${hours} ${strings.hour}${hours > 1 ? "s" : ""} `;
+  if (minutes && track < 2) finalString += `${minutes} ${strings.minute}${minutes > 1 ? "s" : ""}`;
+
+  let finalWords = finalString.trim().split(" ");
+  if (finalWords.length > 5) finalWords[1] += ",";
+  if (finalWords.length > 2) finalWords.splice(-2, 0, strings.and);
+
+  return {
+    nextUpdate: timeLeftms - (timeLeftms % trackUpdate[track]) - trackUpdate[track],
+    humanDiff: finalWords.join(" "),
+  };
 };
