929c22b20ff09449a18ea8952485c140750184e0
radiantly <buildingsomethingfun@gmail.com>
2021-08-15 16:42:45 +0530

    Add sendToDM a global command

--- registerInteractions.js
+++ registerInteractions.js
@@ -8,15 +8,15 @@ import { command as sendToDM } from "./modules/commands/sendToDM.js";
 import config from "./config.js";
 
 const { token, clientId, supportGuildId: guildId } = config;
-const guildCommands = [timerCommand, permissionCheckCommand, remindMeInCommand, sendToDM];
-const globalCommands = [];
+const guildCommands = [timerCommand, permissionCheckCommand, remindMeInCommand];
+const globalCommands = [sendToDM];
+
+const isGlobal = process.argv[2] === "--global";
 
 const rest = new REST({ version: "9" }).setToken(token);
 
-console.log("Started refreshing application (/) commands.");
-console.log(process.argv);
-if (process.argv[2] === "--global")
-  await rest.put(Routes.applicationCommands(clientId), { body: globalCommands });
+console.log(`Started refreshing ${isGlobal ? "global" : "guild"} application commands.`);
+if (isGlobal) await rest.put(Routes.applicationCommands(clientId), { body: globalCommands });
 else await rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: guildCommands });
 
-console.log("Successfully reloaded application (/) commands.");
+console.log("Successful.");
